8f947737a4a4f50d1533653eb9184df1af0947ea
Benjamin Huang <benjaminhuanghuang@gmail.com>
2026-03-24 16:56:47 -0700

    update

--- _Courses/MongoDB Aggregation Pipeline-LinkedIn/mongodb-aggregation/agretate.js
+++ _Courses/MongoDB Aggregation Pipeline-LinkedIn/mongodb-aggregation/agretate.js
@@ -5,10 +5,16 @@ dotenv.config();
 const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri);
 
+const agg = [
+  { $match: { "customer.address.state": "CA" } },
+  { $sort: { total: -1 } },
+  { $limit: 5 },
+];
+
 async function run() {
   try {
     const database = client.db("mongo-study");
-    const result = await database.collection("customers").findOne();
+    const result = await database.collection("orders").aggregate(agg).toArray();
 
     console.log(result);
   } catch (e) {
